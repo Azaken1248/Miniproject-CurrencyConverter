1b74dff8ef48227f8708e2a812f2a870eed0e3ed
Azaken <127621783+Azaken1248@users.noreply.github.com>
2023-11-10 02:04:37 +0530

    Fixed Bug

--- RohanProject/src/CurrencyConverter.cpp
+++ RohanProject/src/CurrencyConverter.cpp
@@ -40,7 +40,7 @@ double getExchangeRate(const std::string& baseCurrency, const std::string& targe
                 std::cout << "Invalid currency codes. Please check your input." << std::endl;
             }
         } else {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",  );
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         }
 
         curl_easy_cleanup(curl);
